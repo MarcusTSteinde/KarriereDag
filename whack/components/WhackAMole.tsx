a511bfda27b0d470b81dc9c66c13044fa4b68f27
Marcus Steinde <marcus.steinde@gmail.com>
2023-11-21 09:55:52 +0100

    feat: Whack-A-Mole mechanics

--- whack/components/WhackAMole.tsx
+++ whack/components/WhackAMole.tsx
@@ -5,38 +5,64 @@ import Score from './Score';
 const WhackAMole = () => {
     const [score, setScore] = useState<number>(0);
     const [moles, setMoles] = useState<boolean[]>(Array(9).fill(false));
+    const [timeLeft, setTimeLeft] = useState<number>(30);
 
-    const showMole = () => {
-        const randomIndex = Math.floor(Math.random() * moles.length);
-        const newMoles = moles.slice();
-        newMoles[randomIndex] = true;
-        setMoles(newMoles);
-    
-        setTimeout(() => {
-            const newMoles = moles.slice();
-            newMoles[randomIndex] = false;
-            setMoles(newMoles);
-        }, 1000); // Hide the mole after 1 second
+    // Hide a specific mole
+    const hideMole = (index: number) => {
+        setMoles(currentMoles => currentMoles.map((mole, moleIndex) => moleIndex === index ? false : mole));
     };
-    
-    useEffect(() => {
-        const interval = setInterval(showMole, 1500); // Show a mole every 1.5 seconds
-        return () => clearInterval(interval); // Clean up the interval on component unmount
-    }, [moles]);
-    
 
+    // Show a random mole
+    useEffect(() => {
+        let moleTimers: NodeJS.Timeout[] = [];
+
+        if (timeLeft > 0) {
+            const randomDelay = Math.random() * (550 - 200) + 200; // Random delay between 300 to 750ms
+            const randomIndex = Math.floor(Math.random() * moles.length);
+            moleTimers = moles.map((_, index) => {
+                
+                return setTimeout(() => {
+                    setMoles(currentMoles => {
+                        if (timeLeft <= 0) return currentMoles; // Don't show new moles if time is up
+                        const newMoles = [...currentMoles];
+                        newMoles[randomIndex] = true;
+
+                        // Hide this mole after some time
+                        setTimeout(() => hideMole(randomIndex), 1000);
+                        return newMoles;
+                    });
+                }, randomDelay); // Randomize appearance time
+            });
+        }
+
+        return () => {
+            moleTimers.forEach(clearTimeout);
+        };
+    }, [timeLeft, moles]);
+
+    // Update timer every second
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (timeLeft > 0) {
+                setTimeLeft(timeLeft - 1);
+            }
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [timeLeft]);
+
+    // Handle mole whack
     const handleWhack = (index: number) => {
         if (moles[index]) {
-            setScore(score + 1);
-            const newMoles = moles.slice();
-            newMoles[index] = false;
-            setMoles(newMoles);
+            setScore(prevScore => prevScore + 1);
+            hideMole(index);
         }
     };
-    
+
     return (
         <div>
-        <Score currentScore={score} />
+            <Score currentScore={score} />
+            <div>Time Left: {timeLeft}</div>
             <div className="game-grid page">
                 {moles.map((mole, index) => (
                     <div key={index} className={`mole-hole ${mole ? 'mole-visible' : ''}`} onClick={() => handleWhack(index)}>
@@ -44,7 +70,7 @@ const WhackAMole = () => {
                     </div>
                 ))}
             </div>
-            </div>
+        </div>
     );
 };
 
