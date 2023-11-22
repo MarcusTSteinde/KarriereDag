17017cbcc12e71402a406321aa011ab23665addd
Ida Christensen <chr.ida@hotmail.com>
2023-11-22 08:15:18 +0100

    removed stuff from marcuses page

--- whack/app/page.tsx
+++ whack/app/page.tsx
@@ -1,15 +1,10 @@
 import WhackAMole from '@/components/WhackAMole'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <WhackAMole></WhackAMole>
-
-      <Link href="/highscores">
-        <p className="text-blue-500 hover:underline cursor-pointer">View High Scores</p>
-      </Link>
     </main>
   )
 }
