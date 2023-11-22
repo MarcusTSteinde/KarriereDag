be3862b959d0908cbb8a97c510b0ab8751076195
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2023-11-22 11:49:31 +0100

    Added form

--- whack/app/page.tsx
+++ whack/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import Login from '@/components/Login'
 import WhackAMole from '@/components/WhackAMole'
 import Image from 'next/image'
 
@@ -5,7 +7,9 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center self-center justify-between bg">
-      <WhackAMole></WhackAMole>
+        <div className='absolute right-2/3 top-1/3'>
+      <Login />
+      </div>
       </div>
     </main>
   )
---
+++ whack/components/Login.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import Input from '@mui/joy/Input';
+import Stack from '@mui/joy/Stack';
+
+export default function InputFormProps() {
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const formJson = Object.fromEntries((formData as any).entries());
+        alert(JSON.stringify(formJson));
+      }}
+    >
+      <Stack spacing={1}>
+        <Input placeholder="Nickname" required />
+        <Input placeholder="Phone number" required />
+        <button type='submit'>Bla bla</button>
+      </Stack>
+    </form>
+  );
+}
