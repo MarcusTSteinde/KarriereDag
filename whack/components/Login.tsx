a3b055c687b41e24cc1ca963375ce021e5cc4326
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2023-11-23 14:15:34 +0100

    checking if user already exists

--- whack/components/Login.tsx
+++ whack/components/Login.tsx
@@ -5,78 +5,96 @@ import Stack from "@mui/joy/Stack";
 import "../styles/login.css";
 import { useEffect, useState } from "react";
 
+interface User {
+  id: number;
+  nickname: string;
+  phoneNumber: string;
+}
+
 export default function InputFormProps() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [nickname, setNickname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://boopabug.azurewebsites.net/api/players"
+        );
 
-useEffect(() => {
-  const fetchUsers = async () => {
+        if (response.ok) {
+          const usersData = await response.json();
+          setUsers(usersData);
+          // You can also store the users in local storage or state as needed
+          localStorage.setItem("users", JSON.stringify(usersData));
+          console.log(users);
+          console.log(usersData); // Log the data structure
+        } else {
+          console.error(
+            `Failed to fetch users with status code: ${response.status}`
+          );
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleSubmit = async (event: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) => {
+    event.preventDefault();
+
+    // Extract data from the form
+    const formData = new FormData(event.currentTarget);
+    const nickname = formData.get("nickname") as string; // Type assertion
+    const phoneNumber = formData.get("phoneNumber");
+
+    // Check if the user with the same nickname already exists
+    if (isUserExisting(nickname)) {
+      alert(
+        "User with the same nickname already exists. Please choose a different nickname."
+      );
+      return;
+    }
+
+    // Your registration logic here (replace with actual API call)
     try {
-      const response = await fetch("https://boopabug.azurewebsites.net/api/players");
+      const response = await fetch(
+        "https://boopabug.azurewebsites.net/api/players",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nickname: nickname,
+            phoneNumber: phoneNumber,
+          }),
+        }
+      );
 
       if (response.ok) {
-        const usersData = await response.json();
-        setUsers(usersData);
-        // You can also store the users in local storage or state as needed
-        localStorage.setItem('users', JSON.stringify(usersData));
-        console.log(users)
-        console.log(usersData); // Log the data structure
-
+        localStorage.setItem("nickname", nickname);
+        alert("Registration successful!");
       } else {
-        console.error(`Failed to fetch users with status code: ${response.status}`);
+        alert(`Registration failed with status code: ${response.status}`);
+        const errorText = await response.text();
+        console.error(errorText);
       }
     } catch (error) {
-      console.error("An error occurred while fetching users:", error);
+      console.error("An error occurred during registration:", error);
     }
   };
 
-  fetchUsers();
-}, []);
-
-const handleSubmit = async (event: {
-  preventDefault: () => void;
-  currentTarget: HTMLFormElement | undefined;
-}) => {
-  event.preventDefault();
-
-  // Extract data from the form
-  const formData = new FormData(event.currentTarget);
-  const nickname = formData.get('nickname') as string; // Type assertion
-  const phoneNumber = formData.get("phoneNumber");
-
-  // Your registration logic here (replace with actual API call)
-  try {
-    const response = await fetch(
-      "https://boopabug.azurewebsites.net/api/players",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          nickname: nickname,
-          phoneNumber: phoneNumber,
-        }),
-      }
-    );
-
-
-
-
-    if (response.ok) {
-      localStorage.setItem('nickname', nickname);
-      alert("Registration successful!");
-      
-    } else {
-      alert(`Registration failed with status code: ${response.status}`);
-      const errorText = await response.text();
-      console.error(errorText);
-    }
-  } catch (error) {
-    console.error("An error occurred during registration:", error);
-  }
-};
-
+  // Check if the user with the same nickname already exists
+  const isUserExisting = (nickname: string) => {
+    return users.some((user) => user.nickname === nickname);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
