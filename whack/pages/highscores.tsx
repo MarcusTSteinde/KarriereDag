ab9f2af7787cd180d07522b74b48e487174de096
Ida Christensen <chr.ida@hotmail.com>
2023-11-23 09:44:33 +0100

    added buttons and check for if user is logged in on buttons

--- whack/pages/highscores.tsx
+++ whack/pages/highscores.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Scoreboard from '@/components/Scoreboard';
 import '../styles/hs.css';
-import Link from 'next/link'
+import Link from 'next/link';
 
 const HighScoresPage: React.FC = () => {
+  const [userNickname, setUserNickname] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Fetch user nickname from local storage on the client side
+    setUserNickname(typeof window !== 'undefined' ? localStorage.getItem('nickname') : null);
+  }, []);
+
   return (
     <main>
       <div className='backbuttonbox'>
@@ -15,7 +22,21 @@ const HighScoresPage: React.FC = () => {
       <img src="/highscores.svg" alt="header high scores" />
       
       <Scoreboard />
-      
+
+      <div className='buttonbox'>
+        <div className='homebutton'>
+          <Link href="/" style={{ textDecoration: 'none' }}>
+            <p className="text-blue-500 hover:underline cursor-pointer">Home Page</p>
+          </Link>
+        </div>
+        {userNickname && (
+          <div className='playagainbutton'>
+            <Link href="/thegame" style={{ textDecoration: 'none' }}>
+              <p className="text-blue-500 hover:underline cursor-pointer">Play Again</p>
+            </Link>
+          </div>
+        )}
+      </div>
     </main>
   );
 };
