153d5bcbc50c0de3b25c97fddb37e976e70e459c
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2023-11-23 13:18:54 +0100

    Login for creating a user

--- whack/components/Login.tsx
+++ whack/components/Login.tsx
@@ -4,27 +4,51 @@ import Input from "@mui/joy/Input";
 import Stack from "@mui/joy/Stack";
 import "../styles/login.css";
 
+
+  const handleSubmit = async (event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
+    event.preventDefault();
+
+    // Extract data from the form
+    const formData = new FormData(event.currentTarget);
+    const nickname = formData.get('nickname');
+    const phoneNumber = formData.get('phoneNumber');
+
+    // Your registration logic here (replace with actual API call)
+    try {
+      const response = await fetch("https://boopabug.azurewebsites.net/api/players", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nickname: nickname,
+          phoneNumber: phoneNumber,
+        }),
+      });
+
+      if (response.ok) {
+        alert("Registration successful!");
+      } else {
+        alert(`Registration failed with status code: ${response.status}`);
+        const errorText = await response.text();
+        console.error(errorText);
+      }
+    } catch (error) {
+      console.error("An error occurred during registration:", error);
+    }
+  }
+
+
 export default function InputFormProps() {
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const formJson = Object.fromEntries((formData as any).entries());
-        alert(JSON.stringify(formJson));
-      }}
-    >
-      <Stack spacing={1} sx={{
-        width: 400
-      }}>
-        <Input placeholder="Nickname" required />
-        <Input placeholder="Phone number" required />
+    <form onSubmit={handleSubmit}>
+      <Stack spacing={1} sx={{ width: 400 }}>
+        <Input name="nickname" placeholder="Nickname" required />
+        <Input name="phoneNumber" placeholder="Phone number" required />
         <div>
-        <button 
-        // type="submit"
-        className="buttonbox">
-          Login
-        </button>
+          <button className="buttonbox">
+            Register
+          </button>
         </div>
       </Stack>
     </form>
