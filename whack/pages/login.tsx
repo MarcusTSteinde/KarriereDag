f89a75bfdf97fe27aacb3d8fd67dbf0848f5f791
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2023-11-23 09:59:53 +0100

    implementing index score

--- whack/pages/login.tsx
+++ whack/pages/login.tsx
@@ -2,22 +2,20 @@ import Login from "@/components/Login";
 import "../app/globals.css";
 import Logo from "../public/logo.svg";
 import Image from "next/image";
-import Scoreboard from "@/components/Scoreboard";
+import IndexScoreboard from "@/components/indexscoreboard";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center self-center justify-between bg">
         <div className="mt-12">
-        <Image src={Logo} alt="game logo" />
+          <Image src={Logo} alt="game logo" />
         </div>
-        <div className="absolute inset-y-0 left-32 w-16 top-80">
-          <Login />
-        </div>
-        <div >
-
-        {/* <Scoreboard /> */}
-
+        <div className="columns-2">
+          <div className="absolute inset-y-0 left-32 w-16 top-80">
+            <Login />
+          </div>
+          <div><IndexScoreboard /></div>
         </div>
       </div>
     </main>
