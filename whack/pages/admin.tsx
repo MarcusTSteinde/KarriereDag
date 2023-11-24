10903ae24cc53141b1df90578643e5e91156dc27
Ida Christensen <chr.ida@hotmail.com>
2023-11-24 10:28:21 +0100

    delete button on admin page

--- whack/pages/admin.tsx
+++ whack/pages/admin.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/hs.css';
 import Link from 'next/link'
 import AdminScoreboard from '@/components/Adminscoreboard';
 
 
 const AdminPage: React.FC = () => {
+    const [deleting, setDeleting] = useState(false);
+
+  const handleDeleteAllPlayers = async () => {
+    try {
+      setDeleting(true);
+
+      const response = await fetch('https://boopabug.azurewebsites.net/api/players/delete-all-players', {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        console.log('All players deleted successfully');
+        window.location.reload();
+      } else {
+        console.error('Failed to delete all players:', response.statusText);
+      }
+    } catch (error) {
+      console.error('An error occurred during the delete operation:', error);
+    } finally {
+      setDeleting(false);
+    }
+  };{handleDeleteAllPlayers}
+
   return (
     <main>
       <div className='backbuttonbox'>
@@ -17,6 +40,10 @@ const AdminPage: React.FC = () => {
       
       <div className="table-container">
         <AdminScoreboard/>
+      </div>
+
+      <div className='deletebutton'>
+        <p onClick={handleDeleteAllPlayers}>Delete All Players</p>
       </div>
       
     </main>
