1308100f8441857344cffd04facf4f7c12bcee6a
Ida Christensen <chr.ida@hotmail.com>
2023-11-27 13:33:30 +0100

    admin button

--- whack/app/page.tsx
+++ whack/app/page.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Logo from "../public/logo.svg";
 import Image from "next/image";
 import IndexScoreboard from "@/components/indexscoreboard";
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,17 +13,27 @@ export default function Home() {
         <div className="mt-12">
           <Image src={Logo} alt="game logo" />
         </div>
+        
         <div className="logo-container2">
-            </div>
-            <div className="logo-container3">
-            </div>
-          <div className="absolute inset-y-0 left-32 top-64">
-            <Login />
-          </div>
-          <div className="absolute inset-y-0 right-64 top-64 w-00">
-            <IndexScoreboard />
         </div>
-      </div>
+
+        <div className="logo-container3">
+        </div>
+
+        <div className="adminbutton">
+          <Link href="/admin" style={{ textDecoration: 'none' }}>
+            <p>Admin Page</p>
+          </Link>
+        </div>
+
+        <div className="absolute inset-y-0 left-32 top-64">
+          <Login />
+        </div>
+
+        <div className="absolute inset-y-0 right-64 top-64 w-00">
+          <IndexScoreboard />
+        </div>
+      </div> 
     </main>
   );
 }
